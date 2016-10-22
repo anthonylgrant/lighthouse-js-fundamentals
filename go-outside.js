942e90e59e682e6681de1d3a7108327d4e885cba
anthonylgrant <anthonylorenzogrant@gmail.com>
2016-10-22 00:09:27 +0000

    Adds conditional excercises

---
+++ go-outside.js
@@ -0,0 +1,45 @@
+// var raining = true;
+// var cold = false;
+
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+
+
+// var cold = false
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+
+
+// var temperature = 12;
+
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+
+var isCitizen = 19;
+var age = 30;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
